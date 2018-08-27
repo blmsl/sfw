848937a25810b8854cc92671a516d7c34f8aa66f
Tarkan Gökdal <tarkan.goekdal@akquinet.de>
2018-08-27 16:24:06 +0200

    team matches information

--- src/app/content/team/team-detail/team-detail-main/team-detail-main.component.ts
+++ src/app/content/team/team-detail/team-detail-main/team-detail-main.component.ts
@@ -6,6 +6,8 @@ import { IClub } from '../../../../shared/interfaces/club/club.interface';
 import { IMediaItem } from '../../../../shared/interfaces/media/media-item.interface';
 import { MediaItemService } from '../../../../shared/services/media/media-item.service';
 import { Observable } from 'rxjs/index';
+import { MatchService } from "src/app/shared/services/match/match.service";
+import { IMatch } from "src/app/shared/interfaces/match/match.interface";
 
 @Component({
   selector: 'team-detail-main',
@@ -22,7 +24,7 @@ export class TeamDetailMainComponent implements OnInit {
   public teamLogo: Observable<IMediaItem>;
   public teamImage: Observable<IMediaItem>;
 
-  constructor(private mediaItemService: MediaItemService) {
+  constructor(private mediaItemService: MediaItemService, private matchService: MatchService) {
   }
 
   ngOnInit() {
@@ -34,6 +36,95 @@ export class TeamDetailMainComponent implements OnInit {
         this.teamImage = this.mediaItemService.getCurrentImage(['teams', 'profile'], this.team.id);
       }
     }
+
+    this.getSeriesForTeam(this.team.id);
+    this.getMatchesForTeam(this.team.id);
   }
 
+  private getMatchesForTeam(teamId: string) {
+    this.matchService.getMatchesWithResult(teamId).subscribe((matches: IMatch[]) => {
+      let defeats: IMatch[] = [];
+      let wins: IMatch[] = [];
+      let draws: IMatch[] = [];
+      let highestWin: IMatch;
+      let highestDefeat: IMatch;
+
+      matches.forEach( (match: IMatch) => {
+        if (match.result.guestTeamGoals != null && match.result.homeTeamGoals != null) {
+          if (match.isHomeTeam) {
+            if (match.result.homeTeamGoals < match.result.guestTeamGoals) {
+              defeats.push(match)
+            } else if (match.result.homeTeamGoals > match.result.guestTeamGoals) {
+              wins.push(match)
+            }
+          } else {
+            if (match.result.guestTeamGoals < match.result.homeTeamGoals) {
+              defeats.push(match)
+            } else if (match.result.guestTeamGoals > match.result.homeTeamGoals) {
+              wins.push(match)
+            }
+          }
+
+          if (match.result.homeTeamGoals === match.result.guestTeamGoals) {
+            draws.push(match)
+          }
+        }
+      });
+
+      highestDefeat = this.highestForArray(defeats);
+      highestWin = this.highestForArray(wins);
+
+      console.log(defeats);
+      console.log(wins);
+      console.log(highestDefeat);
+      console.log(highestWin)
+    })
+  }
+
+  private highestForArray(array: IMatch[]) {
+    return array.reduce( (previousValue, currentValue) => {
+
+      const previousDiff = Math.abs(Number(previousValue.result.guestTeamGoals) - Number(previousValue.result.homeTeamGoals));
+      const currentDiff = Math.abs(Number(currentValue.result.guestTeamGoals) - Number(currentValue.result.homeTeamGoals));
+
+      if (previousDiff > currentDiff) {
+        return previousValue;
+      } else {
+        return currentValue;
+      }
+    });
+  }
+
+  private getSeriesForTeam(teamId: string) {
+    const series: string[] = [];
+
+    this.matchService.getSeriesOfMatches(teamId).subscribe( (matches: IMatch[]) => {
+      matches.forEach( (match: IMatch) => {
+        if (match.result.guestTeamGoals && match.result.homeTeamGoals) {
+          // Defeats
+          if (match.isHomeTeam) {
+            if (match.result.homeTeamGoals < match.result.guestTeamGoals) {
+              series.push("V")
+            } else if (match.result.homeTeamGoals > match.result.guestTeamGoals) {
+              series.push("S")
+            }
+          } else {
+            if (match.result.guestTeamGoals < match.result.homeTeamGoals) {
+              series.push("V")
+            } else if (match.result.guestTeamGoals > match.result.homeTeamGoals) {
+              series.push("S")
+            }
+          }
+
+          // Draws
+          if (match.result.homeTeamGoals === match.result.guestTeamGoals) {
+            series.push("U");
+          }
+        }
+      });
+      console.log(series);
+
+    });
+
+  }
 }
