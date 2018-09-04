15a418c602cef49ad4d3bd7ee3dc8b5bf6b344b3
Thomas Handle <thomas.handle@gmail.com>
2018-09-04 14:53:53 +0200

    calendar settings update

--- src/app/content/setting/settings/settings.component.ts
+++ src/app/content/setting/settings/settings.component.ts
@@ -1,46 +1,50 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { AbstractControl, FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { Title } from '@angular/platform-browser';
-import { IApplication } from '../../../shared/interfaces/application.interface';
+import {
+  Component,
+  OnInit
+}                             from '@angular/core';
+import { ActivatedRoute }     from '@angular/router';
+import {
+  AbstractControl,
+  FormArray,
+  FormBuilder,
+  FormGroup,
+  Validators
+}                             from '@angular/forms';
+import { Title }              from '@angular/platform-browser';
+import { IApplication }       from '../../../shared/interfaces/application.interface';
 import { ApplicationService } from '../../../shared/services/application/application.service';
-import { MatSnackBar } from '@angular/material';
-import { SnackbarComponent } from '../../../shared/components/snackbar/snackbar.component';
-import { IStaticPage } from '../../../shared/interfaces/static-page.interface';
-import { TranslateService } from '@ngx-translate/core';
-import { Observable } from 'rxjs';
-import { ISocialNetwork } from '../../../shared/interfaces/social-network.interface';
-import { debounceTime, distinctUntilChanged } from 'rxjs/operators';
-import { UserService } from '../../../shared/services/user/user.service';
+import { MatSnackBar }        from '@angular/material';
+import { SnackbarComponent }  from '../../../shared/components/snackbar/snackbar.component';
+import { IStaticPage }        from '../../../shared/interfaces/static-page.interface';
+import { TranslateService }   from '@ngx-translate/core';
+import { Observable }         from 'rxjs';
+import { ISocialNetwork }     from '../../../shared/interfaces/social-network.interface';
+import {
+  debounceTime,
+  distinctUntilChanged
+}                             from 'rxjs/operators';
+import { UserService }        from '../../../shared/services/user/user.service';
 
 @Component({
   templateUrl: './settings.component.html'
 })
 export class SettingsComponent implements OnInit {
 
-  /* @HostListener('window:beforeunload')
-  canDeactivate(): Observable<boolean> | boolean {
-    return JSON.stringify(this.application).toLowerCase() === JSON.stringify(this.savedApplication).toLowerCase();
-  } */
-
   public application: IApplication;
   public form: FormGroup;
   public selectedStaticPage: AbstractControl = null;
-  public shorteningProviders: {
-    title: string,
-    key: string
-  }[] = [];
+
   public link: string = 'http://www.google.de';
   public savedApplication: IApplication;
   public roles: string[];
 
   constructor(private fb: FormBuilder,
-    private route: ActivatedRoute,
-    public snackBar: MatSnackBar,
-    private title: Title,
-    private translateService: TranslateService,
-    private userService: UserService,
-    private applicationService: ApplicationService) {
+              private route: ActivatedRoute,
+              public snackBar: MatSnackBar,
+              private title: Title,
+              private translateService: TranslateService,
+              private userService: UserService,
+              private applicationService: ApplicationService) {
     this.roles = userService.getUserRoles();
   }
 
@@ -52,13 +56,12 @@ export class SettingsComponent implements OnInit {
 
     this.form = this.fb.group({
       page: this.fb.group({
-        name: [this.application.page.name, [Validators.required, Validators.minLength(10), Validators.maxLength(100)]],
+        name: [ this.application.page.name, [ Validators.required, Validators.minLength(10), Validators.maxLength(100) ] ],
         description: this.application.page.description,
         email: this.application.page.email,
         title: this.application.page.title,
         assignedKeywords: this.application.assignedKeywords
       }),
-      // urlShortening: this.application.urlShortening,
       registration: this.application.registration,
       downtime: this.fb.group({
         isEnabled: this.application.downtime.isEnabled,
@@ -71,7 +74,7 @@ export class SettingsComponent implements OnInit {
         teamOfTheMonth: this.application.mailing ? this.application.mailing.teamOfTheMonth : null,
         memberOfTheWeek: this.application.mailing ? this.application.mailing.memberOfTheWeek : null,
         newPublishedArticle: this.application.mailing ? this.application.mailing.newPublishedArticle : null,
-        newCreatedMatch: this.application.mailing ? this.application.mailing.newCreatedMatch : null,
+        newCreatedMatch: this.application.mailing ? this.application.mailing.newCreatedMatch : null
       }),
       assignedCalendars: this.initCalendars()
     });
@@ -93,7 +96,7 @@ export class SettingsComponent implements OnInit {
     const formArray = [];
     if (this.application.assignedCalendars) {
       for (let i = 0; i < this.application.assignedCalendars.length; i++) {
-        formArray.push(this.initCalendar(this.application.assignedCalendars[i]));
+        formArray.push(this.initCalendar(this.application.assignedCalendars[ i ]));
       }
     }
     return this.fb.array(formArray);
@@ -101,27 +104,26 @@ export class SettingsComponent implements OnInit {
 
   initCalendar(calendar: { title: string, link: string }): FormGroup {
     return this.fb.group({
-      link: [calendar ? calendar.link : '', [Validators.required]],
-      title: [calendar ? calendar.title : '', [Validators.required]]
+      link: [ calendar ? calendar.link : '', [ Validators.required ] ],
+      title: [ calendar ? calendar.title : '', [ Validators.required ] ]
     });
   }
 
   addCalendar() {
-    const control = <FormArray>this.form.controls['assignedCalendars'];
+    const control = <FormArray>this.form.controls[ 'assignedCalendars' ];
     control.push(this.initCalendar({ title: '', link: '' }));
   }
 
   deleteCalendar(i: number) {
-    console.log(i);
-    const formArray = <FormArray>this.form.controls['assignedCalendars'];
-    formArray.controls.splice(i, 1);
+    const control = <FormArray>this.form.get('assignedCalendar');
+    control.removeAt(i);
   }
 
   initStaticPages(): FormArray {
     const formArray = [];
     if (this.application.staticPages) {
       for (let i = 0; i < this.application.staticPages.length; i++) {
-        formArray.push(this.initStaticPage(this.application.staticPages[i]));
+        formArray.push(this.initStaticPage(this.application.staticPages[ i ]));
       }
     }
     return this.fb.array(formArray);
@@ -129,10 +131,10 @@ export class SettingsComponent implements OnInit {
 
   initStaticPage(staticPage: IStaticPage): FormGroup {
     return this.fb.group({
-      isEnabled: [staticPage ? staticPage.isEnabled : false],
-      assignedCategories: [staticPage ? staticPage.assignedCategories : '', [Validators.required]],
-      text: [staticPage ? staticPage.text : '', [Validators.required]],
-      title: [staticPage ? staticPage.title : '', [Validators.required]]
+      isEnabled: [ staticPage ? staticPage.isEnabled : false ],
+      assignedCategories: [ staticPage ? staticPage.assignedCategories : '', [ Validators.required ] ],
+      text: [ staticPage ? staticPage.text : '', [ Validators.required ] ],
+      title: [ staticPage ? staticPage.title : '', [ Validators.required ] ]
     });
   }
 
@@ -152,7 +154,7 @@ export class SettingsComponent implements OnInit {
       };
       const addCtrl = this.initStaticPage(staticPage);
       control.push(addCtrl);
-      this.setSelectedStaticPage(control.controls[control.length - 1]);
+      this.setSelectedStaticPage(control.controls[ control.length - 1 ]);
     });
   }
 
@@ -173,7 +175,7 @@ export class SettingsComponent implements OnInit {
     const formArray = [];
     if (this.application.social) {
       for (let i = 0; i < this.application.social.length; i++) {
-        formArray.push(this.initSocialProvider(this.application.social[i]));
+        formArray.push(this.initSocialProvider(this.application.social[ i ]));
       }
     }
     return this.fb.array(formArray);
@@ -181,13 +183,13 @@ export class SettingsComponent implements OnInit {
 
   initSocialProvider(provider: ISocialNetwork): FormGroup {
     return this.fb.group({
-      link: [provider ? provider.link : '', [Validators.required]],
-      title: [provider ? provider.title : '', [Validators.required]]
+      link: [ provider ? provider.link : '', [ Validators.required ] ],
+      title: [ provider ? provider.title : '', [ Validators.required ] ]
     });
   }
 
   addSocialProvider(): void {
-    const control = <FormArray>this.form.controls['social'];
+    const control = <FormArray>this.form.controls[ 'social' ];
     const provider: ISocialNetwork = {
       link: '',
       title: ''
@@ -197,25 +199,25 @@ export class SettingsComponent implements OnInit {
   }
 
   removeSocialProvider(i: number): void {
-    const control = <FormArray>this.form.controls['social'];
+    const control = <FormArray>this.form.controls[ 'social' ];
     control.removeAt(i);
   }
 
   saveSettings() {
     this.applicationService.updateApplication(this.application.id, this.application).then(() => {
-      // set Page Title
-      if (this.title.getTitle() !== this.application.page.title) {
-        this.title.setTitle(this.application.page.title);
-      }
+        // set Page Title
+        if (this.title.getTitle() !== this.application.page.title) {
+          this.title.setTitle(this.application.page.title);
+        }
 
-      this.snackBar.openFromComponent(SnackbarComponent, {
-        data: {
-          status: 'success',
-          message: 'general.applications.updateMessage'
-        },
-        duration: 2500
-      });
-    },
+        this.snackBar.openFromComponent(SnackbarComponent, {
+          data: {
+            status: 'success',
+            message: 'general.applications.updateMessage'
+          },
+          duration: 2500
+        });
+      },
       (error: any) => {
         this.snackBar.openFromComponent(SnackbarComponent, {
           data: {
