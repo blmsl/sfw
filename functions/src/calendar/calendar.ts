cea4860d8c850ea5a75e72278c0fd670474a198e
Thomas Handle <thomas.handle@gmail.com>
2018-09-06 20:21:54 +0200

    update

--- functions/src/calendar/calendar.ts
+++ functions/src/calendar/calendar.ts
@@ -1,23 +1,67 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import * as moment from 'moment';
 
 const { google } = require('googleapis');
 
-const calendar = google.calendar({ version: 'v3' });
-/*
-const cors = require('cors')({
-  origin: true
-});*/
+const GOOGLE_API_KEY = functions.config().google.calendar.key;
+const calendar = google.calendar({ version: 'v3', auth: GOOGLE_API_KEY });
 
-export const getGoogleCalendarEvents = functions.region('europe-west1').https.onRequest((req, resp) => {
+const db = admin.firestore();
 
+const currentDate = moment();
+const timeMin = currentDate.subtract(1, 'year').toISOString();
+const timeMax = currentDate.add(2, 'year').toISOString();
 
-  return calendar.events.list({
-    calendarId: '41q3u1q8pfh26dm1lpkuh7lsrs@group.calendar.google.com'
-  }, (err, res) => {
-    if (err)
-      return resp.status(401).send(err.message);
+export const getGoogleCalendarEvents = functions.region('europe-west1').https.onRequest(async (req, resp) => {
 
-    return resp.status(200).send(res.json());
-  });
+  const eventList = [];
+
+  try {
+    const applicationRef = db.collection('applications');
+    const activeAppRef = await applicationRef.where('isCurrentApplication', '==', true).get();
+    if (activeAppRef.size === 0) {
+      resp.send('No current Application found');
+    }
+
+    const promises = [];
+    for (let cal of activeAppRef.docs[0].data().assignedCalendars) {
+      console.log(cal.link);
+      promises.push(getEventList(cal.link));
+    }
+
+    const snapshots = await Promise.all(promises);
+
+    snapshots.forEach(snap => {
+        eventList.push(snap.data().items);
+    });
+
+    return resp.send(eventList);
+
+  } catch (error) {
+    console.log(error);
+    return resp.status(500).send(error);
+  }
 
 });
+
+function getEventList(cal): Promise<any> {
+  return calendar.events.list({
+    calendarId: cal.link,
+    timeMin: timeMin,
+    timeMax: timeMax,
+    // maxResults: 100,
+    singleEvents: true,
+    orderBy: 'startTime'
+  });
+}
+
+/*
+, (err, res) => {
+    if (err) {
+      console.log(cal.link + ' ' + err);
+      return [];
+    }
+    return res.data.items;
+  })
+ */
