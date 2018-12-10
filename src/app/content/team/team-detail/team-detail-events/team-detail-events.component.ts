78beb3466efe574b76be8fc74697a49ac7a82d5f
Thomas Handle <thomas.handle@gmail.com>
2018-12-10 14:23:12 +0100

    fix team-events

--- src/app/content/team/team-detail/team-detail-events/team-detail-events.component.ts
+++ src/app/content/team/team-detail/team-detail-events/team-detail-events.component.ts
@@ -1,5 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ITeam } from '../../../../shared/interfaces/team/team.interface';
+import { Observable } from 'rxjs/index';
+import { IArticle } from '../../../../shared/interfaces/article.interface';
+import { ArticleService } from '../../../../shared/services/article/article.service';
 
 @Component({
   selector: 'team-detail-events',
@@ -10,7 +13,10 @@ export class TeamDetailEventsComponent implements OnInit {
 
   @Input() team: ITeam;
 
-  constructor() {
+  public articles$: Observable<IArticle[]>;
+
+  constructor(private articleService: ArticleService) {
+    this.articles$ = articleService.articles$;
   }
 
   ngOnInit() {
